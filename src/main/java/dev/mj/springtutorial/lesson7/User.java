5dbf267d583a7c8f00ebe23596f297957126ec6f
Michał Jaśkiewicz <michal.grzegorz.jaskiewicz@gmail.com>
2017-09-05 18:52:27 +0200

    Lesson 6 - Own factory methods.

---
+++ src/main/java/dev/mj/springtutorial/lesson7/User.java
@@ -0,0 +1,18 @@
+package dev.mj.springtutorial.lesson7;
+
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@NoArgsConstructor(access = PRIVATE)
+public class User {
+    private String name;
+    private int age;
+
+    public static User getInstance(String name, int age) {
+        User user = new User();
+        user.name = name;
+        user.age = age;
+        return user;
+    }
+}
