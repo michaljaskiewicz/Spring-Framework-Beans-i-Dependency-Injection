b2c785b78c37c762ad7aa48c81e609c486b2cca3
Michał Jaśkiewicz <michal.grzegorz.jaskiewicz@gmail.com>
2017-09-05 18:40:14 +0200

    Lesson 5 - Creating beans using factory methods.

---
+++ src/main/java/dev/mj/springtutorial/lesson6/MainStaticFactoryMethod.java
@@ -0,0 +1,17 @@
+package dev.mj.springtutorial.lesson6;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class MainStaticFactoryMethod {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("config_lesson6.xml");
+        Calendar calendar = (Calendar) context.getBean("calendar");
+        DateFormat formatter = (DateFormat) context.getBean("formatter");
+        System.out.println(formatter.format(calendar.getTime()));
+    }
+}
