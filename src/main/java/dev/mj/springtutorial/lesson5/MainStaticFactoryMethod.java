c0f7eaf841342186f433e642732aa2794e6fad6d
Michał Jaśkiewicz <michal.grzegorz.jaskiewicz@gmail.com>
2017-09-12 00:14:39 +0200

    Renamed config files for lessons 5-7. Added dependency to log4j.

--- src/main/java/dev/mj/springtutorial/lesson5/MainStaticFactoryMethod.java
+++ src/main/java/dev/mj/springtutorial/lesson5/MainStaticFactoryMethod.java
@@ -9,7 +9,7 @@ import java.util.Calendar;
 public class MainStaticFactoryMethod {
 
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("config_lesson5.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("lesson5.xml");
         Calendar calendar = (Calendar) context.getBean("calendar");
         DateFormat formatter = (DateFormat) context.getBean("formatter");
         System.out.println(formatter.format(calendar.getTime()));
--- src/main/java/dev/mj/springtutorial/lesson6/Main.java
+++ src/main/java/dev/mj/springtutorial/lesson6/Main.java
@@ -6,6 +6,6 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
 
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("config_lesson6.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("lesson6.xml");
     }
 }
