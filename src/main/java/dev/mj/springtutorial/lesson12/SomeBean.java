8880e564a9ed792feda275959a1ac864beb182b0
Michał Jaśkiewicz <michal.grzegorz.jaskiewicz@gmail.com>
2017-09-18 07:49:50 +0200

    Lesson 12 - maps and properties injection.

---
+++ src/main/java/dev/mj/springtutorial/lesson12/SomeBean.java
@@ -0,0 +1,15 @@
+package dev.mj.springtutorial.lesson12;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Locale;
+import java.util.Map;
+import java.util.Properties;
+
+@Getter
+@Setter
+public class SomeBean {
+    private Map<String, Locale> locales;
+    private Properties settings;
+}
