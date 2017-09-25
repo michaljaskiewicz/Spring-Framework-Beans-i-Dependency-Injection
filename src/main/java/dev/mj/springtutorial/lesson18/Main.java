8e101e1e556d19a8c62bcc048baab98251130ae2
Michał Jaśkiewicz <michal.grzegorz.jaskiewicz@gmail.com>
2017-09-25 07:37:02 +0200

    Lesson18 - change scope from public to package-private.

--- src/main/java/dev/mj/springtutorial/lesson18/Main.java
+++ src/main/java/dev/mj/springtutorial/lesson18/Main.java
@@ -3,7 +3,7 @@ package dev.mj.springtutorial.lesson18;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class Main {
+class Main {
 
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("lesson18.xml");
